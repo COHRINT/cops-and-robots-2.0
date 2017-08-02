fixed softmax regression models

src/softmaxModels.py
@@ -603,27 +603,18 @@ class Softmax:
 			post.addG(Gaussian(mu,var,g.weight*np.exp(logCHat))); 
 		return post; 
 
-	def pointEval2D(self,softClass,point):
-		#Evaluates the function at a point in 2D
-
-		top = np.exp(self.weights[softClass][0]*point[0] + self.weights[softClass][1]*point[1]); 
-		bottom = 0; 
-		for i in range(0,self.size):
-			bottom += np.exp(self.weights[i][0]*point[0] + self.weights[i][1]*point[1]); 
-		return top/bottom; 
-
 	def pointEvalND(self,softClass,point):
 		#Evaluates the function at a point in any dimensionality. 
 		topIn = 0;
 		for i in range(0,len(self.weights[0])):
 			topIn+=self.weights[softClass][i]*point[i]; 
-		top = np.exp(topIn); 
+		top = np.exp(topIn+self.bias[softClass]); 
 		bottom = 0; 
 		for i in range(0,self.size):
 			bottomIn = 0; 
 			for j in range(0,len(self.weights[0])):
 				bottomIn += self.weights[i][j]*point[j]; 
-			bottom+=np.exp(bottomIn); 
+			bottom+=np.exp(bottomIn + self.bias[i]); 
 		return top/bottom; 
 
 	def plot1D(self,low=0,high = 5,res = 100,labels = None,vis = True):
@@ -1058,23 +1049,26 @@ def testMakeNear():
 	plt.show(); 
 
 def testLogisticRegression():
-	X = [[1,3],[2,2]]; 
-	t = [0,1]; 
+	X = [[1,3],[2,4],[2,2],[4,3]]; 
+	t = [0,0,1,1]; 
 	cols = ['r','b','g','y','w','k','m']; 
 	a = Softmax(); 
 	a.logRegress(X,t,1); 
+	#a.plot2D(vis = True); 
 	[x,y,c] = a.plot2D(vis = False); 
+
 
 	plt.contourf(x,y,c); 
 	for i in range(0,len(X)):
 		plt.scatter(X[i][0],X[i][1],c=cols[t[i]]); 
-	plt.show(); 
 
-	
-	testPoint = [1,2]; 
+
+	testPoint = [1,1]; 
 	winPercent = a.pointEvalND(1,testPoint); 
 	lossPercent = a.pointEvalND(0,testPoint); 
 	print(winPercent,lossPercent); 
+	plt.show(); 
+
 
 
 if __name__ == "__main__":